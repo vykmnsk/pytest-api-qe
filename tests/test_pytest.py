d2c8ad012c3941e395e6de8c59dd37d5f41ad4e0
vyk <>
2022-03-10 14:03:15 +1100

    Add sanity tests for a typical API endpoint

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,2 +1,50 @@
-def test_pytest_shows_err():
-    assert 1 == 0
+import os
+import json
+import pytest
+import requests
+
+
+API_URL = os.getenv('TEST_API_URL')
+
+
+
+def test_response_code():
+    resp = call_get()
+    assert resp.ok
+
+
+def test_response_not_empty():
+    resp = call_get()
+    assert resp.content, 'empty response'
+
+
+def test_response_is_json():
+    resp = call_get()
+    parse_json(resp)
+
+
+def test_response_content():
+    resp = call_get()
+    jcontent = parse_json(resp)
+    assert jcontent['surname']
+
+
+def test_post_call_rejected():
+    resp = call_post()
+    assert not resp.ok
+
+
+def call_get():
+    return requests.get(API_URL)
+
+
+def call_post():
+    return requests.post(API_URL, data={})
+
+
+def parse_json(resp):
+    try:
+        result = json.loads(resp.content)
+    except ValueError:
+        pytest.fail(f'response is not JSON: {resp.text[0:1000]}')
+    return result
