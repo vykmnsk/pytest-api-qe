90ce68ac6c1c994c83f958d89e71446b74d8817f
vyk <>
2022-03-11 14:38:11 +1100

    Test JSON response models

--- tests/test_content.py
+++ tests/test_content.py
@@ -1,7 +1,28 @@
 import common
 
 
-def test_response_content(get_response):
+def test_model_musicfestival(get_response):
+    jcontent = extract_json(get_response)
+    for festival in jcontent:
+        name = festival.get('name')
+        assert name and name.strip(), f"Expect name in festival={festival}"
+        bands = festival.get('bands')
+        assert bands, f"Expect bands in festival={festival}"
+
+
+def test_model_band(get_response):
+    jcontent = extract_json(get_response)
+    for festival in jcontent:
+        bands = festival.get('bands')
+        assert bands, f"Expect bands in festival={festival}"
+        for band in bands:
+            name = band.get('name')
+            assert name and name.strip(), f"Expect name in band={band}"
+            rec_label = band.get('recordLabel')
+            assert rec_label and rec_label.strip(), f"Expect recordLabel in band={band}"
+
+
+def extract_json(get_response):
     jcontent = common.parse_json(get_response)
-    assert 'name' in jcontent
-    assert 'bands' in jcontent
+    assert len(jcontent) > 0, f"Expect non-emply list in response={get_response.text}"
+    return jcontent
